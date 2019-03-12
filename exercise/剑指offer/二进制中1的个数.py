4043bc8515a07bde201022144044ed031140a734
stormchasingg <3130102803@zju.edu.cn>
2019-03-12 23:12:29 +0800

    Create 二进制中1的个数.py

---
+++ exercise/剑指offer/二进制中1的个数.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def NumberOf1(self, n):
+        # write code here
+        if n < 0:
+            n = n & 0xffffffff
+        count = 0
+        while n != 0:
+            count += 1
+            n &= n - 1
+        return count
