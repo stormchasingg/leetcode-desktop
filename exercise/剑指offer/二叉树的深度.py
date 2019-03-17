0aa36b9d2ba3eb6c14b27abe33dd40e1b16887b0
stormchasingg <3130102803@zju.edu.cn>
2019-03-17 22:59:45 +0800

    Create 二叉树的深度.py

---
+++ exercise/剑指offer/二叉树的深度.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def TreeDepth(self, pRoot):
+        # write code here
+        if not pRoot:
+            return 0
+        left = self.TreeDepth(pRoot.left)
+        right = self.TreeDepth(pRoot.right)
+        res = max(left, right) + 1
+        return res
