1097ae23140ad486af0a0f1aad705ad718159b24
stormchasingg <3130102803@zju.edu.cn>
2019-04-03 22:27:17 +0800

    Delete 不用加减乘除做加法.py

--- exercise/剑指offer/不用加减乘除做加法.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-class Solution:
-    def Add(self, num1, num2):
-        # write code here
-        while num2 != 0:
-            tmp = num1 ^ num2
-            num2 = (num1 & num2) << 1
-            num1 = tmp & 0xffffffff
-        return num1 if num1 >> 31 == 0 else num1 - 2**32
