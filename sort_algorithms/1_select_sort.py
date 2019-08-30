96c71d618f6aaf438be742a269632a148b7ef7c9
stormchasingg <3130102803@zju.edu.cn>
2019-08-30 10:27:55 +0800

    Update 1_select_sort.py

--- sort_algorithms/1_select_sort.py
+++ sort_algorithms/1_select_sort.py
@@ -1,1 +1,14 @@
-
+def select_sort(nums):
+    for i in range(len(nums)-1):
+        index = i
+        for j in range(i+1, len(nums)):
+            if nums[j] < nums[index]:
+                index = j
+        if index != i:
+            nums[i], nums[index] = nums[index], nums[i]
+            
+            
+if __name__ == '__main__':
+    nums = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    select_sort(nums)
+    print(nums)
