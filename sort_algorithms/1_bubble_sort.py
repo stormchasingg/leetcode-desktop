4658514abc912bb53570c0fc10ab23c528f1337d
stormchasingg <3130102803@zju.edu.cn>
2019-08-30 09:54:27 +0800

    Update 1_bubble_sort.py

--- sort_algorithms/1_bubble_sort.py
+++ sort_algorithms/1_bubble_sort.py
@@ -1,1 +1,11 @@
+def bubble_sort(nums):
+    for i in range(len(nums)-1, 0, -1):
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
 
+
+if __name__ == '__main__':
+    nums = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    bubble_sort(nums)
+    print(nums)
