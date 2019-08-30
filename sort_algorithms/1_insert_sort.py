260b03bce344153f53f7c07af1e65134de4bb7a1
stormchasingg <3130102803@zju.edu.cn>
2019-08-30 10:03:59 +0800

    Update 1_insert_sort.py

--- sort_algorithms/1_insert_sort.py
+++ sort_algorithms/1_insert_sort.py
@@ -1,1 +1,11 @@
+def insert_sort(nums):
+    for i in range(1, len(nums)):
+        for j in range(i, 0, -1):
+            if nums[j] < nums[j-1]:
+                nums[j], nums[j-1] = nums[j-1], nums[j]
 
+
+if __name__ == '__main__':
+    nums = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    insert_sort(nums)
+    print(nums)
