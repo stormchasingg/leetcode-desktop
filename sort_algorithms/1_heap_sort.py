eb430c7829b79fae4febfd151a20d2a348394313
stormchasingg <3130102803@zju.edu.cn>
2019-08-31 22:30:22 +0800

    Update 1_heap_sort.py

--- sort_algorithms/1_heap_sort.py
+++ sort_algorithms/1_heap_sort.py
@@ -1,4 +1,4 @@
-def adjust_heap(nums, i):
+def adjust_heap(nums, i, size):
     lchild = 2 * i + 1
     rchild = 2 * i + 2
     largest = i
@@ -11,19 +11,19 @@ def adjust_heap(nums, i):
             nums[largest], nums[i] = nums[i], nums[largest]
             adjust_heap(nums, largest, size)
 
-            
-def build_heap(nums):
+
+def build_heap(nums, size):
     for i in range(size // 2, -1, -1):
         adjust_heap(nums, i, size)
-        
+
 
 def heap_sort(nums):
-    global size = len(nums)
-    for i in range(size, -1, -1):
+    size = len(nums)
+    for i in range(size-1, -1, -1):
         nums[0], nums[i] = nums[i], nums[0]
-        adjust_heap(nums, 0)
-        
-        
+        adjust_heap(nums, 0, i)
+
+
 if __name__ == '__main__':
     nums = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     heap_sort(nums)
