a813d8a92be482cf63397e6253f789f44a542fea
stormchasingg <3130102803@zju.edu.cn>
2019-09-13 21:19:21 +0800

    Create 2_binary_search_tree.py

---
+++ sort_algorithms/2_binary_search_tree.py
@@ -0,0 +1,45 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class BST:
+    def __init__(self, nums):
+        self.root = TreeNode(nums[0])
+        for num in nums[1:]:
+            self.insert(num)
+
+    def insert(self, num):
+        flag, node, parent = self.search(self.root, self.root, num)
+        if not flag:
+            if num < parent.val:
+                parent.left = TreeNode(num)
+            elif num > parent.val:
+                parent.right = TreeNode(num)
+
+    def search(self, node, parent, num):
+        if not node:
+            return False, node, parent
+        if num == node.val:
+            return True, node, parent
+        elif num < node.val:
+            return self.search(node.left, node, num)
+        elif num > node.val:
+            return self.search(node.right, node, num)
+
+
+def inorder_traversal(root):
+    if not root:
+        return []
+    left = inorder_traversal(root.left)
+    right = inorder_traversal(root.right)
+    return left + [root.val] + right
+
+
+if __name__ == '__main__':
+    nums = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    binary_search_tree = BST(nums)
+    res = inorder_traversal(binary_search_tree.root)
+    print(res)
